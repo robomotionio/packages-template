8d8681056de845bb503d40826873c210a9b93cbf
Faik Uygur <faik@robomotion.io>
2024-07-14 22:30:59 +0300

    disconnect

---
+++ src/github-python/nodes/disconnect.py
@@ -0,0 +1,37 @@
+from robomotion.node import Node
+from robomotion.decorators import register_node
+from robomotion.variable import InVariable
+from robomotion.message import Context
+
+from common import api_key_store
+from icon import github_icon
+
+@register_node(name='Robomotion.GitHub.Connect', title='Connect', color='#0D4082', icon=github_icon)
+class Disconnect(Node):
+    def __init__(self):
+        super().__init__()
+
+        # Input
+        self.in_conn_id = InVariable(
+            title='Connection Id',
+            type='String',
+            scope='Message',
+            name='connection_id',
+            customScope=True,
+            messageScope=True
+        )
+
+    def on_create(self):
+        return
+
+    def on_message(self, ctx: Context):
+
+        conn_id = self.in_conn_id.get(ctx)
+        if not conn_id:
+            raise ValueError("Connection Id can not be empty")
+
+       
+        api_key_store.delete_api_key(conn_id)
+
+    def on_close(self):
+        return
